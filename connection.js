505296e8410c36d3b85cb9fa7aa15e2b1f3c308b
Glitch (glitch-hello-website) <none>
2022-07-04 16:03:56 +0000

    ☎️🏛 Checkpoint ./application.js:24923914/2742 ./connection.js:24923914/434

--- connection.js
+++ connection.js
@@ -1172,6 +1172,9 @@ class Client {
 
         cursorX = this.serverX(cursorX);
         cursorY = this.serverY(cursorY);
+          
+      window.connection.send(window.buffers.mousePosition(this.cursorX, this.cursorY));
+
         const c = this.createView(13);
         c.setUint8(0, 16);
         c.setInt32(1, cursorX, true);
